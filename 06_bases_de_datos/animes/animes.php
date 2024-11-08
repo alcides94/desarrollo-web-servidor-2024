c52fd4b99c1987f8ed9be47fc1845b50ff3df4bb
alcides94 <alcidesquinterosugarte94@gmail.com>
2024-11-08 12:00:21 +0100

    tema bases de datos

---
+++ 06_bases_de_datos/animes/animes.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <?php
+    /**CODIGO DE ERROR */
+        error_reporting( E_ALL );
+        ini_set( "display_errors", 1 );
+        require('../conexion.php')
+    ?>
+    <style>
+        .table-primary{
+            --bs-table-color-state:green;
+            --bs-table-bg:beige;
+        }
+    </style>
+</head>
+<body>
+
+    <div class="container">
+        <?php
+            $sql= "SELECT * FROM animes";
+            $resultado = $_conexion -> query($sql);
+        ?>
+        <table class="table table-striped">
+            <thead class="table-primary">
+                <tr>
+                    <th>Titulo</th>
+                    <th>Estudio</th>
+                    <th>Año</th>
+                    <th>Numero de Temporadas</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                //fetch tatra a resultado como un array
+                    while($fila=$resultado -> fetch_assoc()){
+                ?>
+                    <tr>
+                        <td> <?php  echo $fila["titulo"] ?> </td>
+                        <td> <?php  echo $fila["nombre_estudio"] ?> </td>
+                        <td> <?php  echo $fila["anno_estreno"] ?> </td>
+                        <td> <?php  echo $fila["num_temporadas"] ?> </td>
+                    </tr>
+                
+                <?php    }
+                ?>
+               
+            </tbody>
+        </table>
+
+
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
