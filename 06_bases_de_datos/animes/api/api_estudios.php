e47f5fbe127b073e1777b7f420175ba02e7942dc
alcides94 <alcidesquinterosugarte94@gmail.com>
2025-01-14 13:17:10 +0100

    api

--- 06_bases_de_datos/animes/api/api_estudios.php
+++ 06_bases_de_datos/animes/api/api_estudios.php
@@ -25,4 +25,40 @@
             break;
     }
 
+    function manejarGet($_conexion){
+        //echo json_encode(["metodo" => "get"]);
+        $sql="SELECT * FROM estudios";
+        $stmt =$_conexion -> prepare($sql);
+        $stmt -> execute();
+        $resultado = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($resultado);
+    }
+    function manejarPost($_conexion, $entrada){
+        //echo json_encode(["metodo" => "post"]);
+        $sql ="INSERT INTO estudios VALUES(:nombre_estudio, :ciudad, :anno_fundacion)";
+        $stmt =$_conexion -> prepare($sql);
+        
+        $stmt -> execute([
+            "nombre_estudio" => $entrada["nombre_estudio"],
+            "ciudad" => $entrada["ciudad"],
+            "anno_fundacion" => $entrada["anno_fundacion"]
+        ]);
+        
+        if ($stmt) {
+            echo json_encode(["mensaje"=>"el estudio se ha creado"]);
+        }else{
+            echo json_encode(["mensaje"=>"error al crear"]);
+        }
+
+
+    }
+    
+    function manejarPut($_conexion, $entrada){
+        echo json_encode(["metodo" => "put"]);
+    }
+    function manejarDelete($_conexion, $entrada){
+        echo json_encode(["metodo" => "delete"]);
+    }
+    
+
 ?>
--- 06_bases_de_datos/animes/api/conexion_pdo.php
+++ 06_bases_de_datos/animes/api/conexion_pdo.php
@@ -1,21 +1,15 @@
 <?php
-    $_servidor ="localhost"; //o 127.0.0.1 (loopback)
-    $_usuario="estudiante"; //usuario de la bases de datos
-    $_contrasena ="estudiante"; //cantraseña de la bases de datos
-    $_base_de_datos="animes_bd"; //bases de datos que vamos a usar
+ 
+    $_servidor = "localhost";
+    $_usuario = "estudiante";
+    $_contrasena = "estudiante";
+    $_base_de_datos = "animes_bd";
 
-    try{
-        $_conexion = new PDO("mysql:host=$_servidor;dbname=$_base_de_datos,
-        $_usuario,$_contrasena");
-
+    try {
+        $_conexion = new PDO("mysql:host=$_servidor;dbname=$_base_de_datos",
+            $_usuario,$_contrasena);
         $_conexion -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-
-    }catch(PDOException $e){
-        die("conexion fallida: " . $e -> getMessage());
+    } catch (PDOException $e) {
+        die("Conexión fallida: " . $e -> getMessage());
     }
-
-
-
-
 ?>
